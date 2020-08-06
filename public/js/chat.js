e079a56b8f44b989217a3ff2ade6ff2bb31ac166
kacperluk <kacper.luk@o2.pl>
2020-08-06 17:33:33 +0200

    Form and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,24 +1,37 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $locationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const msg = e.target.elements.message.value
 
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     socket.emit('sendMessage', msg, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error)
         }
 
         console.log('Message delivered!')
     })
-    e.target.elements.message.value = ''
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
+    $locationButton.setAttribute('disabled', 'disabled')
     if (!navigator.geolocation) {
         return alert('Your browser does not support your browser')
     }
@@ -28,6 +41,7 @@ document.querySelector('#send-location').addEventListener('click', () => {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $locationButton.removeAttribute('disabled')
             console.log('location shared')
         })
     })

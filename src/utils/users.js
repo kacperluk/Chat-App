21bd6202ddbd188291d595ba0e8831e796571762
kacperluk <kacper.luk@o2.pl>
2020-08-08 16:49:06 +0200

    Storing Users: Part I

---
+++ src/utils/users.js
@@ -0,0 +1,53 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            'error': 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((usr) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in user!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+addUser({
+    id: 99,
+    username: 'name',
+    room: 'room1'
+})
+
+console.log(users)
+
+const removedUser = removeUser(99)
+console.log(removedUser)
+console.log(users)

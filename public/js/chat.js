9c9f26e9220bec2f3a982af6857c49dc0d6c0a43
kacperluk <kacper.luk@o2.pl>
2020-08-08 15:13:28 +0200

    Timestamps for Location Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,10 +21,10 @@ socket.on('message', (message) => {
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('locationMessage', (url) => {
-    console.log(url)
+socket.on('locationMessage', (message) => {
     const html = Mustache.render(locationTemplate, {
-        url: url
+        url: message.url,
+        createdAt: moment(message.createdAt).format('H:mm:ss')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })

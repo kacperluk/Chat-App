19c2b6c58c8e2e0b06462ca143631e739b0037eb
kacperluk <kacper.luk@o2.pl>
2020-08-08 12:34:43 +0200

    Rendering Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,11 +4,19 @@ const socket = io()
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
+const $messages = document.querySelector('#messages')
 
 const $locationButton = document.querySelector('#send-location')
 
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
